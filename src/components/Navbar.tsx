6e142651c48ec94d47fd8408599f11e11266790f
Taufan Prasetyo <taufanremote@gmail.com>
2024-10-12 10:30:10 +0700

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+
+const navLinks = [
+    { title: 'About', path: '#about' },
+    { title: 'Portfolio', path: '#portfolio' },
+]
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const toggleNav = () => setNav(!nav)
+    const closeNav = () => setNav(false)
+    const menuVariants = {
+        open: {
+            x: 0,
+            transition: {
+                stiffness: 20,
+                damping: 15,
+            },
+        },
+        closed: {
+            x: '-100%',
+            transition: {
+                stiffness: 20,
+                damping: 15,
+            },
+        },
+    }
+
+    return (
+        <div className='text-white/70 pt-6'>
+            <div className='hidden md:flex items-center px-4 py-2 mx-auto max-w-[400px]'>
+                <ul className='flex flex-row p-4 space-x-8'>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <p>
+                                {link.title}
+                            </p>
+                        </li>
+                    ))}
+
+                    <li>
+                        <a href="#contact" className='group'>
+                            <h1 className='text-lg font-bold text-white/70 cursor-pointer'>Contact Me</h1>
+                            <div className='relative'>
+                                <div className='absolute w-2/3 h-1 transition-all duration-300 ease-out bg-orange-400 rounded-full group-hover:w-full' />
+                                <div className='mt-1 absolute w-2/3 h-1 transition-all duration-300 ease-out bg-orange-600 rounded-full group-hover:w-full' />
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div onClick={toggleNav} className='md:hidden absolute right-5 top-5 rounded border border-white/70 p-2 z-50 text-white/70'>
+                {
+                    nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />
+                }
+            </div>
+
+            <motion.div
+                initial={false}
+                animate={nav ? 'open' : 'closed'}
+                variants={menuVariants}
+                className='fixed left-0 top-0 w-full z-40 bg-black/90'
+            >
+                <ul className='text-4xl font-semibold my-24 text-center space-y-8'>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link href={link.path} onClick={closeNav}>
+                                {link.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Navbar
